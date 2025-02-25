17d977db7a1651efc71ee3e3987126af3b9331f4
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-02-24 21:40:51 -0800

    Add projectDict, update project name and add todoObj to todoDict

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+projectDict = {'default':[]};
+
 function createTodo (title, description="", dueDate="", priority="", completeStatus="Incomplete", project="default") {
 
     const getTitle = () => title;
@@ -21,37 +23,61 @@ function createTodo (title, description="", dueDate="", priority="", completeSta
     };
 
     const getProject = () => project;
-    const updateProject = (newProject) => project = newProject;
-
-    return { getTitle, updateTitle, getDescription, updateDescription, getDueDate, updateDueDate, getPriority, updatePriority, getCompleteStatus, changeCompleteStatus, getProject, updateProject};
-}
-
-function createProject (name) {
-    projectList = []
-    const getName = () => name;
-
-    projectList = []
-    const addToProjectList = () => {
-        projectList.push(name);
+    const changeProject = (newProject) => {
+        project = newProject;
     }
-    const getProjectList = () => projectList;
 
-    return { getName, addToProjectList, getProjectList }
+    return { getTitle, updateTitle, getDescription, updateDescription, getDueDate, updateDueDate, getPriority, updatePriority, getCompleteStatus, changeCompleteStatus, getProject, changeProject};
 }
 
-projectDict = {}
-function manageProjectDict (todoObj) {
-    project = todoObj.getProject()
 
-    if (projectDict[proj]) {
-        projectDict[proj].push(todoObj);
+function createProject (newProjectName) {
+    if (checkIfProjectExists(newProjectName)) {
+        return "Error: Project name already exists";
     } else {
-        projectDict[proj] = [todoObj];
+        projectDict[newProjectName] = []
+        return "Project created";
     }
 }
+
+function checkIfProjectExists (projectName) {
+    if (projectDict[projectName]) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function updateProject(todoObj, projectName) {
+    if (checkIfProjectExists(projectName)) {
+        todoObj.changeProject(projectName);
+        addToProjectDict(todoObj);
+    }
+    else {
+        return "Error: Project does not exist"
+    }
+}
+
+function addToProjectDict (todoObj) {
+    projectDict[todoObj.getProject()].push(todoObj);
+}
+
 
 const todo1 = createTodo('Buy groceries', 'Milk, eggs, and bread', '2025-02-25', 'High');
 const todo2 = createTodo('Read book', 'Finish reading JavaScript book', '2025-02-26', 'Medium');
 
+// Todo Object Testing
 console.log(todo1.getTitle()); // Output: 'Buy groceries'
 console.log(todo2.getDescription()); // Output: 'Finish reading JavaScript book'
+
+
+// Project Testing
+console.log(todo1.getProject());
+console.log(updateProject(todo1, 'BigNewProject'));  // error project does not exist
+createProject('BigNewProject'); // create project
+updateProject(todo1, 'BigNewProject') // change update project
+// addToProjectDict(todo1);  //add todo to project dict
+console.log(todo1.getProject());
+addToProjectDict(todo2);
+console.log(projectDict['default'][0].getTitle()); // returns 'Read book'
+console.log(projectDict['default'][todo2].getTitle()); // returns error
