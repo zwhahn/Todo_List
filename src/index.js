4b4964a829f77b92d7138f4ee753624badd66422
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-03-14 15:31:52 -0700

    Establish two column layout, import into JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles.css";
+
 projectDict = {'default':[]};
 
 function createTodo (title, description="", dueDate="", priority="", completeStatus="Incomplete", project="default") {
@@ -73,23 +75,22 @@ function getTodoByTitle (projectName, todoTitle) {
     
 }
 
+// const todo1 = createTodo('Buy groceries', 'Milk, eggs, and bread', '2025-02-25', 'High');
+// const todo2 = createTodo('Read book', 'Finish reading JavaScript book', '2025-02-26', 'Medium');
 
-const todo1 = createTodo('Buy groceries', 'Milk, eggs, and bread', '2025-02-25', 'High');
-const todo2 = createTodo('Read book', 'Finish reading JavaScript book', '2025-02-26', 'Medium');
-
-// Todo Object Testing
-console.log(todo1.getTitle()); // Output: 'Buy groceries'
-console.log(todo2.getDescription()); // Output: 'Finish reading JavaScript book'
+// // Todo Object Testing
+// console.log(todo1.getTitle()); // Output: 'Buy groceries'
+// console.log(todo2.getDescription()); // Output: 'Finish reading JavaScript book'
 
 
-// Project Testing
-console.log(todo1.getProject());
-console.log(updateProject(todo1, 'BigNewProject'));  // error project does not exist
-createProject('BigNewProject'); // create project
-updateProject(todo1, 'BigNewProject') // change update project
-// addToProjectDict(todo1);  //add todo to project dict
-console.log(todo1.getProject());
-addToProjectDict(todo2);
-console.log(projectDict['default'][0].getTitle()); // returns 'Read book'
-// console.log(projectDict['default'][todo2].getTitle()); // returns error
-console.log(getTodoByTitle('default', 'Read book')); // returns todo object with that title
+// // Project Testing
+// console.log(todo1.getProject());
+// console.log(updateProject(todo1, 'BigNewProject'));  // error project does not exist
+// createProject('BigNewProject'); // create project
+// updateProject(todo1, 'BigNewProject') // change update project
+// // addToProjectDict(todo1);  //add todo to project dict
+// console.log(todo1.getProject());
+// addToProjectDict(todo2);
+// console.log(projectDict['default'][0].getTitle()); // returns 'Read book'
+// // console.log(projectDict['default'][todo2].getTitle()); // returns error
+// console.log(getTodoByTitle('default', 'Read book')); // returns todo object with that title
