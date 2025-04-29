9343a2a0742464136533bda57a2c11885cf9d3c8
zwhahn <117476820+zwhahn@users.noreply.github.com>
2025-04-28 20:27:28 -0700

    Delete test cases

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -108,23 +108,3 @@ export const TodoApp = (function() {
     };
 
 })();
-
-// const todo1 = createTodo('Buy groceries', 'Milk, eggs, and bread', '2025-02-25', 'High');
-// const todo2 = createTodo('Read book', 'Finish reading JavaScript book', '2025-02-26', 'Medium');
-
-// // Todo Object Testing
-// console.log(todo1.getTitle()); // Output: 'Buy groceries'
-// console.log(todo2.getDescription()); // Output: 'Finish reading JavaScript book'
-
-
-// // Project Testing
-// console.log(todo1.getProject());
-// console.log(updateProject(todo1, 'BigNewProject'));  // error project does not exist
-// createProject('BigNewProject'); // create project
-// updateProject(todo1, 'BigNewProject') // change update project
-// // addTodoToProject(todo1);  //add todo to project dict
-// console.log(todo1.getProject());
-// addTodoToProject(todo2);
-// console.log(projectDict['default'][0].getTitle()); // returns 'Read book'
-// // console.log(projectDict['default'][todo2].getTitle()); // returns error
-// console.log(getTodoByTitle('default', 'Read book')); // returns todo object with that title
